ae071238dd55326ff4bae4a2746de99e574817f3
LakshithaMadushan <LakshithaMadushan@users.noreply.github.com>
2019-03-13 17:11:45 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+declare var XLSX;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'xlsx-read';
+
+  constructor() {
+    console.log('load xlsx');
+  }
+
+  Upload() {
+    const fileUpload = (document.getElementById('fileUpload') as HTMLInputElement);
+
+    const regex = /^([a-zA-Z0-9\s_\\.\-:])+(.xls|.xlsx)$/;
+    if (regex.test(fileUpload.value.toLowerCase())) {
+      if (typeof (FileReader) !== 'undefined') {
+        const reader = new FileReader();
+
+        if (reader.readAsBinaryString) {
+          reader.onload = (e) => {
+            this.ProcessExcel(reader.result);
+          };
+          reader.readAsBinaryString(fileUpload.files[0]);
+        }
+      } else {
+        alert('This browser does not support HTML5.');
+      }
+    } else {
+      alert('Please upload a valid Excel file.');
+    }
+  }
+
+  ProcessExcel(data) {
+    const workbook = XLSX.read(data, {
+      type: 'binary'
+    });
+
+    const firstSheet = workbook.SheetNames[0];
+
+    const excelRows = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[firstSheet]);
+    console.log(excelRows);
+  }
 }
